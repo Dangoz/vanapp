84471013257a03c237b5fc5c31badfc4139e607f
Dangoz <71613713+Dangoz@users.noreply.github.com>
2022-04-04 18:04:57 -0700

    13

---
+++ ChatHub/MessageHub.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Server.Models;
+using Server.Types;
+using Server.Data;
+using Microsoft.AspNetCore.SignalR;
+using Server.Extensions;
+
+namespace Server.ChatHub
+{
+    public class MessageHub : Hub
+    {
+        private readonly MessageRepository _messageRepository;
+        private readonly UserRepository _userRepository;
+        public MessageHub(MessageRepository messageRepository,UserRepository userRepository)
+        {
+            _userRepository = userRepository;
+            _messageRepository = messageRepository;
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            var httpContext = Context.GetHttpContext();
+            var otherUser = httpContext.Request.Query["user"].ToString();
+
+            var groupName = GetGroupName(Context.User.GetUsername(), otherUser);
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+            var group = await AddToGroup(groupName);
+
+            var messages = await _messageRepository.
+                GetMessageThread(Context.User.GetUsername(), otherUser);
+
+            await Clients.Caller.SendAsync("ReceiveMessageThread", messages);
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            var group = await RemoveFromMessageGroup();
+            await base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task SendMessage(MessageType message_obj) {
+            Console.WriteLine("222222222222222");
+            var username = Context.User.GetUsername();
+
+            var sender = await _userRepository.GetUserByUsernameAsync(username);
+            var recipient = await _userRepository.GetUserByUsernameAsync(message_obj.RecipientUsername);
+
+            if (recipient == null) throw new HubException("Not found user");
+
+            var message = new Message
+            {
+                Sender = sender,
+                Recipient = recipient,
+                SenderUsername = sender.UserName,
+                RecipientUsername = recipient.UserName,
+                Content = message_obj.Message
+            };
+
+            var groupName = GetGroupName(sender.UserName, recipient.UserName);
+
+            var group = await _messageRepository.GetMessageGroup(groupName);
+
+            var savedMsg = _messageRepository.AddMessage(message);
+            var saveResult = await _messageRepository.SaveAllAsync();
+
+                var msgResponse = new
+                {
+                    senderUsername = sender.UserName,
+                    messageSent = savedMsg.MessageSent,
+                    messageContent = message_obj.Message
+                };
+                await Clients.Group(groupName).SendAsync("NewMessage", msgResponse);
+            
+        }
+
+        private async Task<Group> AddToGroup(string groupName)
+        {
+            var group = await _messageRepository.GetMessageGroup(groupName);
+            var connection = new Connection(Context.ConnectionId, Context.User.GetUsername());
+
+            if (group == null)
+            {
+                group = new Group(groupName);
+                _messageRepository.AddGroup(group);
+            }
+
+            group.Connections.Add(connection);
+
+            if (await _messageRepository.SaveAllAsync()) return group;
+
+            throw new HubException("Failed to join group");
+        }
+
+        private async Task<Group> RemoveFromMessageGroup()
+        {
+            var group = await _messageRepository.GetGroupForConnection(Context.ConnectionId);
+            var connection = group.Connections.FirstOrDefault(x => x.ConnectionId == Context.ConnectionId);
+            _messageRepository.RemoveConnection(connection);
+            if (await _messageRepository.SaveAllAsync()) return group;
+
+            throw new HubException("Failed to remove from group");
+        }
+
+        private string GetGroupName(string caller, string other)
+        {
+            var stringCompare = string.CompareOrdinal(caller, other) < 0;
+            return stringCompare ? $"{caller}-{other}" : $"{other}-{caller}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,109 @@
+using Server.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Server.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Text.Json.Serialization;
+using Server.Services;
+using Server.ChatHub;
+using Server.Types;
+
+var builder = WebApplication.CreateBuilder(args);
+
+ConfigurationManager Configuration = builder.Configuration;
+
+var conStr = Configuration.GetConnectionString("DefaultConnection");
+
+    builder.Services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings"));
+
+    builder.Services.AddScoped<UserRepository>();
+    builder.Services.AddScoped<PhotoService>();
+    builder.Services.AddScoped<MessageRepository>();
+
+    builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(conStr));
+
+    builder.Services.AddControllers().AddJsonOptions(x =>
+                x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+    builder.Services.AddCors();
+
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    builder.Services.AddIdentityCore<SiteUser>(opt => 
+    {
+        opt.Password.RequireNonAlphanumeric = false;
+    })
+    .AddRoles<Role>()
+    .AddRoleManager<RoleManager<Role>>()
+    .AddSignInManager<SignInManager<SiteUser>>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options => {
+            options.TokenValidationParameters = new TokenValidationParameters {
+                ValidateIssuerSigningKey = true, 
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JwtKey"])),
+                ValidateIssuer = false, 
+                ValidateAudience = false, 
+            };
+
+            options.Events = new JwtBearerEvents
+            {
+                OnMessageReceived = context =>
+                {
+                    var accessToken = context.Request.Query["access_token"];
+
+                    var path = context.HttpContext.Request.Path;
+                    if (!string.IsNullOrEmpty(accessToken) &&
+                        path.StartsWithSegments("/hubs"))
+                    {
+                        context.Token = accessToken;
+                    }
+                    return Task.CompletedTask;
+                }
+            };
+        });
+
+builder.Services.AddSignalR();
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseRouting();
+
+app.UseCors(x => x.AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowCredentials()
+    .WithOrigins("http://localhost:3000"));
+
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+// app.MapControllers();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    endpoints.MapHub<MessageHub>("hubs/message");
+    endpoints.MapFallbackToController("Index", "StaticWeb");
+});
+
+
+using (var scope = app.Services.CreateScope()) {
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<ApplicationDbContext>();
+
+    context.Database.Migrate();
+}
+
+app.Run();
